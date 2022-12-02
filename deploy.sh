fa2c0f63d9d864e3cd3f907092ae496fdf0c4012
Gabriel Casas <casasmgb@gmail.com>
2022-12-01 23:01:32 -0400

    correccion de problema con sudo

--- deploy.sh
+++ deploy.sh
@@ -2,29 +2,29 @@
 #!/bin/bash -e
 (
 	echo "INFO: Executing installation script!"
-	#sh install-docker.sh
+	#sh install-sudo docker.sh
 )
 
 # Verify that we can at least get version output
-if ! docker --version; then
-	echo "ERROR: Did Docker get installed?"
-  install-docker.sh
+if ! sudo docker --version; then
+	echo "ERROR: Did sudo Docker get installed?"
+  install-sudo docker.sh
 fi
 
 { # try
   echo " *** Erase current deployed version"
-  docker stop ci-cd-grupal3-prod ci-cd-grupal3-preprod 
-  docker container rm ci-cd-grupal3-prod ci-cd-grupal3-preprod
+  sudo docker stop ci-cd-grupal3-prod ci-cd-grupal3-preprod 
+  sudo docker container rm ci-cd-grupal3-prod ci-cd-grupal3-preprod
   echo " *** Getting last version of deployed project"
-  docker pull casasmgb/ci-cd-grupal3:latest 
+  sudo docker pull casasmgb/ci-cd-grupal3:latest 
   echo " *** Started the images for prod and preprod"
-  docker run --name ci-cd-grupal3-prod -it -d -p 3001:3000 casasmgb/ci-cd-grupal3:latest 
-  docker run --name ci-cd-grupal3-preprod -it -d -p 3002:3000 casasmgb/ci-cd-grupal3:latest 
+  sudo docker run --name ci-cd-grupal3-prod -it -d -p 3001:3000 casasmgb/ci-cd-grupal3:latest 
+  sudo docker run --name ci-cd-grupal3-preprod -it -d -p 3002:3000 casasmgb/ci-cd-grupal3:latest 
 
 } || { # catch
   echo " *** Erasen container because fauled process"
-  docker stop ci-cd-grupal3-prod ci-cd-grupal3-preprod 
-  docker container rm ci-cd-grupal3-prod ci-cd-grupal3-preprod
+  sudo docker stop ci-cd-grupal3-prod ci-cd-grupal3-preprod 
+  sudo docker container rm ci-cd-grupal3-prod ci-cd-grupal3-preprod
 }
 
 echo " *** Verify services for production are correct running"
@@ -34,4 +34,4 @@ echo " *** Verify services for pre production are correct running"
 curl http://localhost:3002
 echo "DONE...!"
 
-echo "INFO: Successfully verified docker installation!"
+echo "INFO: Successfully verified sudo docker installation!"
