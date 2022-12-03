73aad770b06f78d4c9bf3a6019ad6200f1357b67
Gabriel Casas <casasmgb@gmail.com>
2022-12-02 20:28:54 -0400

    cambio para probar despliegue

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World Group 3! From PROD');
+      expect(appController.getHello()).toBe('Hello World Group 3! From PROD - Deployed');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Group 3! From PROD';
+    return 'Hello World Group 3! From PROD - Deployed';
   }
 }
